43b5139520b50edb5d3d1cc86b876768decaba7f
PrinceZahahaMojiJiJee <75375585+PrinceZahahaMojiJiJee@users.noreply.github.com>
2021-02-11 12:27:20 +0700

    Add files via upload

--- Project/src/main.c
+++ Project/src/main.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "light_sensor.h"
 #include "temp_sensor.h"
+#include "ultrasonic.h"
 #include <RTL.h>
 
 OS_TID front_light, fan;
@@ -9,20 +10,31 @@ __task void fan_control();
 
 bool dark = false;
 float temp;
+bool is_on = false;
+bool passed = false;
+void house_light_control();
 
 int main()
 {
 	DWT_Init();
 	DS1820_GPIO_Configure();
 	motor_config();
+	
 	set_up_light_sensor();
 	set_up_light();
 	light_off();
+	
 	SystemClock_Config();
+	TIM_GPIO_Config();
+	TIMBase_Config();
+	set_up_house_light();
+	house_light_off();
+	
 	while(1)
 	{
 		front_light_control();
 		fan_control();
+		house_light_control();
 	}
 }
 
@@ -51,5 +63,29 @@ void fan_control()
 	else
 	{
 		motor_off();
+	}
+}
+void house_light_control()
+{
+	if(getdistance() < 0.2)
+	{
+		if(passed)
+		{
+			if(!is_on)
+			{
+				house_light_on();
+				is_on = true;
+			}
+			else
+			{
+				house_light_off();
+				is_on = false;
+			}
+			passed = false;
+		}
+	}
+	else
+	{
+		passed = true;
 	}
 }
